e613c1febdac72e3ca2145ebbcbda2a8a8de677b
Edward <Edward.thomson9@gmail.com>
2022-07-12 10:53:57 +1000

    Migrated from main ChibiOS fork

---
+++ os/common/startup/ARMCMx/compilers/GCC/mk/startup_MIMXRT1021.mk
@@ -0,0 +1,22 @@
+
+# List of the ChibiOS IMXRT1021 startup and CMSIS files.
+STARTUPSRC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt1.c \
+			 $(CHIBIOS_CONTRIB)/os/common/startup/ARMCMx/devices/MIMXRT102x/crt2_imxrt1021.c \
+			 $(CHIBIOS_CONTRIB)/os/common/ext/NXP/MIMXRT102x/system_MIMXRT1021.c
+          
+STARTUPASM = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt0_v7m.S \
+             $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/vectors.S
+
+STARTUPINC = $(CHIBIOS)/os/common/portability/GCC \
+             $(CHIBIOS)/os/common/ext/ARM/CMSIS/Core/Include \
+             $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC \
+             $(CHIBIOS_CONTRIB)/os/common/startup/ARMCMx/devices/MIMXRT102x \
+             $(CHIBIOS_CONTRIB)/os/common/ext/NXP/MIMXRT102x
+
+STARTUPLD  = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/ld
+STARTUPLD_CONTRIB  = $(CHIBIOS_CONTRIB)/os/common/startup/ARMCMx/compilers/GCC/ld
+
+# Shared variables
+ALLXASMSRC += $(STARTUPASM)
+ALLCSRC    += $(STARTUPSRC)
+ALLINC     += $(STARTUPINC)
