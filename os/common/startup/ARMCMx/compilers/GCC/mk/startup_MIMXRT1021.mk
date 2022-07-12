03fbab359db2fcfaf37a4600762d5d623f3dbe54
Edward <Edward.thomson9@gmail.com>
2022-07-12 15:52:12 +1000

    Adding initial boot.[sh] files and implementations

--- os/common/startup/ARMCMx/compilers/GCC/mk/startup_MIMXRT1021.mk
+++ os/common/startup/ARMCMx/compilers/GCC/mk/startup_MIMXRT1021.mk
@@ -1,11 +1,12 @@
 
 # List of the ChibiOS IMXRT1021 startup and CMSIS files.
 STARTUPSRC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt1.c \
-			 $(CHIBIOS_CONTRIB)/os/common/startup/ARMCMx/devices/MIMXRT102x/crt2_imxrt1021.c \
 			 $(CHIBIOS_CONTRIB)/os/common/ext/NXP/MIMXRT102x/system_MIMXRT1021.c
+			 # $(CHIBIOS_CONTRIB)/os/common/startup/ARMCMx/devices/MIMXRT102x/crt2_imxrt1021.c \
           
 STARTUPASM = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt0_v7m.S \
-             $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/vectors.S
+             $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/vectors.S \
+			 $(CHIBIOS_CONTRIB)/os/common/startup/ARMCMx/devices/boot.S
 
 STARTUPINC = $(CHIBIOS)/os/common/portability/GCC \
              $(CHIBIOS)/os/common/ext/ARM/CMSIS/Core/Include \
